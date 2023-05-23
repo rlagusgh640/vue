18107d6b37b9654dfd253b7054cd0172c050d96e
rlagusgh640 <66357554+rlagusgh640@users.noreply.github.com>
2023-05-23 16:23:44 +0900

    Add files via upload

---
+++ vuetify1/src/router/index.js
@@ -0,0 +1,41 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import HomeView from '../views/HomeView.vue'
+import Team from '../views/Team.vue'
+import MyProject from '../views/MyProject.vue'
+import Education from '../views/Education.vue'
+Vue.use(VueRouter)
+
+const routes = [
+  {
+    path: '/',
+    name: 'home',
+    component: HomeView
+  },
+  {
+    path: '/myproject',
+    name: 'MyProject',
+    component:MyProject
+  
+  },
+  {
+    path: '/team',
+    name: 'Team',
+    component:Team
+  
+  },
+  {
+    path: '/education',
+    name: 'Education',
+    component:Education
+  
+  }
+]
+
+const router = new VueRouter({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes
+})
+
+export default router
